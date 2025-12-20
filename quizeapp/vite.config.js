7a986d1ff3bbb00c4e4a4bbcf8d4670e50bc19e3
AZ <amiraelzaian204@gmail.com>
2025-12-20 12:31:17 +0200

    feat: Add server configuration to Vite for local development

--- quizeapp/vite.config.js
+++ quizeapp/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // يسمع على كل الـ IPs المحلية
+    port: 5173, // تقدر تغيّري البورت لو تحبي
+  },
 });
